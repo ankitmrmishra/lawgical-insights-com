547011508e88b2c22f7910fc5fa45f9f066a6392
Sanskaari <ankitmrmishra1118@gmail.com>
2023-12-07 00:09:21 +0530

    kwjbfwuefuwifbwyfbwef

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: "process.env.NEXT_PUBLIC_SANITY_PROJECT_ID",
+    projectId: "3xdg0q0d",
     dataset: "production",
   },
 });
