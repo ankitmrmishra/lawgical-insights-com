bb8f847e8bc30000da9036fecf881c0032327f90
Sanskaari <ankitmrmishra1118@gmail.com>
2023-12-06 23:49:24 +0530

    eoife

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = "production";
 const apiVersion = "2023-01-01";
 
