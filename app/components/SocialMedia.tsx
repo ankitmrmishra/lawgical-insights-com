bf5f1fe53002d2cf227b50e42972b1c82d6f4a19
Sanskaari <ankitmrmishra1118@gmail.com>
2024-01-02 19:14:34 +0530

    fixed twitter link

--- app/components/SocialMedia.tsx
+++ app/components/SocialMedia.tsx
@@ -18,7 +18,7 @@ const SocialMedia = () => {
           <SlSocialInstagram />
         </span>
       </a>
-      <a href='http://x.com/mandvitriptahi8' target='_blank'>
+      <a href='http://x.com/mandvitripathi8' target='_blank'>
         <span className='w-10 h-10 text-xl bg-LightestNavy rounded-full inline-flex items-center justify-center hover:text-Green cursor-pointer hover:-translate-y-2 transition-all duration-300'>
           <SlSocialTwitter />
         </span>
